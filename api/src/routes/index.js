6c7f764b921b79c996142899f2a3d594ace4b332
unknown <serocontacto@gmail.com>
2023-01-15 14:39:50 -0300

    commit prueba

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+const countryRoute = require('./countries');
+const activityRoute = require('./activities');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/countries', countryRoute);
+router.use('/activities', activityRoute);
 
 
 module.exports = router;
